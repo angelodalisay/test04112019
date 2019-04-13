bca01ca1b13e0db57fcc452e44377dc07fca99cf
Angelo Dalisay <dalisay.angelo@gmail.com>
2019-04-13 17:01:34 -0600

    initial push

---
+++ frontend/src/app/api.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Task } from './task';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  PHP_API_SERVER = "http://127.0.0.1:8888";
+
+  readTasks(): Observable<Task[]>{
+    return this.httpClient.get<Task[]>(`${this.PHP_API_SERVER}/api/read.php`);
+  }
+  createTask(task: Task): Observable<Task>{
+    return this.httpClient.post<Task>(`${this.PHP_API_SERVER}/api/create.php`, task);
+  }
+
+  updateTask(task: Task){
+    return this.httpClient.put<Task>(`${this.PHP_API_SERVER}/api/update.php`, task);   
+  }
+  deleteTask(id: number){
+    return this.httpClient.delete<Task>(`${this.PHP_API_SERVER}/api/delete.php/?id=${id}`);
+  }
+  constructor(private httpClient: HttpClient) { }
+}
---
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Task } from '../task';
+import { Validators } from '@angular/forms';
+// import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+})
+
+export class DashboardComponent implements OnInit {
+
+  tasks:  Task[];
+  selectedTask:  Task  = { id :  null , name:null, description:  null};
+  private taskshow = false;
+  private editshow = false;
+  constructor(
+    private apiService: ApiService,
+    ) { }
+
+  createTaskShow() {
+    this.taskshow = true;
+    this.editshow = false;
+  }
+  editTaskShow() {
+    this.editshow = true;
+    this.taskshow = false;
+  }
+
+  createTask(form) {
+      // Create
+      this.apiService.createTask(form.value).subscribe((tasks: Task)=> {
+      this.fetchData();
+      form.reset();
+      alert("Task Created!");
+      });
+  }
+  updateTask(form){
+    // Update
+    if(this.selectedTask && this.selectedTask.id){
+      form.value.id = this.selectedTask.id;
+      this.apiService.updateTask(form.value).subscribe((tasks: Task)=> {});
+      this.fetchData();
+      alert("Task Updated!");
+    } else {
+
+    }
+
+  }
+
+  selectTask(task: Task){
+    this.selectedTask = task;
+    this.editshow = true;
+    this.taskshow = false;
+  }
+
+  deleteTask(id){
+    this.apiService.deleteTask(id).subscribe((tasks: Task)=>{
+      this.fetchData();
+      alert("Task Completed!");
+    });
+
+  }
+
+  ngOnInit() {
+      this.fetchData();
+  }
+
+  fetchData() {
+    this.apiService.readTasks().subscribe((tasks: Task[])=> {
+      this.tasks = tasks;
+    });
+  }  
+}
